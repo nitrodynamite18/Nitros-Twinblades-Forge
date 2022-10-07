1c539728625eb18a3b2398dffba2a2ed8bc2956a
nitro_dynamite18 <nitrodynamite18@gmail.com>
2022-10-07 16:35:14 -0400

    Sweeping hitbox for Infernal Twinblade should be twice as wide.

--- src/main/java/com/kyanite/nitro/nitros_twinblades/registry/items/custom/InfernalSwordItem.java
+++ src/main/java/com/kyanite/nitro/nitros_twinblades/registry/items/custom/InfernalSwordItem.java
@@ -3,10 +3,14 @@ package com.kyanite.nitro.nitros_twinblades.registry.items.custom;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -20,6 +24,11 @@ public class InfernalSwordItem extends SwordItem {
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
         components.add(Component.translatable("tooltip.nitros_twinblades.infernal_sword"));
         super.appendHoverText(stack, level, components, flag);
+    }
+
+    @Override
+    public AABB getSweepHitBox(@NotNull ItemStack stack, @NotNull Player player, @NotNull Entity target){
+        return target.getBoundingBox().inflate(1.0D, 0.5D, 1.0D);
     }
 
     @Override
